180a32fe6dffe301e2f59c020c9c29f2991c2796
현수 <104980470+znight1020@users.noreply.github.com>
2022-11-12 21:06:10 +0900

    ProductImage 컴포넌트(using react-image-gallery)

--- client/src/components/views/DetailProductPage/DetailProductPage.js
+++ client/src/components/views/DetailProductPage/DetailProductPage.js
@@ -1,7 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-
+import ProductImage from "./Sections/ProductImage";
+import ProductInfo from "./Sections/ProductInfo";
+import { Row, Col } from "antd";
 /*
  * react-router-dom 버전 6부터는 element로 컴포넌트를 만들고,
  * route props(match, history, location)을 받지 않는다.
@@ -11,19 +13,40 @@ import axios from "axios";
  */
 function DetailProductPage(props) {
     const { productId } = useParams();
+    const [Product, setProduct] = useState({});
+
     useEffect(() => {
         axios
             .get(`/api/product/products_by_id?id=${productId}&type=single`)
             .then((response) => {
                 if (response.data.success) {
-                    console.log("response.data", response.data);
+                    //console.log("response.data", response.data);
+                    setProduct(response.data.product[0]);
                 } else {
                     alert("상세 정보 가져오기를 실패했습니다.");
                 }
             });
     }, []);
 
-    return <div>DetailProductPage</div>;
+    return (
+        <div style={{ width: "100%", padding: "3rem 4rem" }}>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+                <h1>{Product.title}</h1>
+            </div>
+
+            <br />
+            <Row gutter={[16, 16]}>
+                <Col lg={12} sm={24}>
+                    {/* productImage*/}
+                    <ProductImage detail={Product} />
+                </Col>
+                <Col lg={12} sm={24}>
+                    {/* productInfo*/}
+                    <ProductInfo />
+                </Col>
+            </Row>
+        </div>
+    );
 }
 
 export default DetailProductPage;
---
+++ client/src/components/views/DetailProductPage/Sections/ProductImage.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import ImageGallery from "react-image-gallery";
+
+export default function ProductImage(props) {
+    const [Images, setImages] = useState([]);
+    useEffect(() => {
+        if (props.detail.images && props.detail.images.length > 0) {
+            let images = [];
+
+            props.detail.images.map((item) => {
+                images.push({
+                    original: `http://localhost:5000/${item}`,
+                    thumbnail: `http://localhost:5000/${item}`,
+                });
+            });
+            setImages(images);
+        }
+    }, [props.detail]);
+    // const images = [
+    //     {
+    //         original: "https://picsum.photos/id/1018/1000/600/",
+    //         thumbnail: "https://picsum.photos/id/1018/250/150/",
+    //     },
+    //     {
+    //         original: "https://picsum.photos/id/1015/1000/600/",
+    //         thumbnail: "https://picsum.photos/id/1015/250/150/",
+    //     },
+    //     {
+    //         original: "https://picsum.photos/id/1019/1000/600/",
+    //         thumbnail: "https://picsum.photos/id/1019/250/150/",
+    //     },
+    // ];
+
+    return (
+        <div>
+            <ImageGallery items={Images} />
+        </div>
+    );
+}
