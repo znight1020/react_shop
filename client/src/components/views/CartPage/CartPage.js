f71bb532cdcfc7bbb8607a991f13ed91cbcc8795
현수 <104980470+znight1020@users.noreply.github.com>
2022-11-26 15:50:25 +0900

    Update CartPage.js

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -23,24 +23,6 @@ function CartPage(props) {
         }
     }, [props.user.userData]);
 
-    // useEffect(() => {
-    //     let cartItems = [];
-    //     // 리덕스 User State안에 Cart 안에 상품이 들어있는지 확인
-    //     if (props.user.cartDetail) {
-    //         if (props.user.cartDetail.product.length > 0) {
-    //             props.user.cartDetail.product.forEach((item) => {
-    //                 cartItems.push(item.id);
-    //             });
-
-    //             dispatch(
-    //                 getCartItems(cartItems, props.user.cartDetail.product)
-    //             ).then((response) => {
-    //                 console.log(response);
-    //             });
-    //         }
-    //     }
-    // }, [props.user.cartDetail]);
-
     return (
         <div style={{ width: "85%", margin: "3rem auto" }}>
             <h1>My Cart</h1>
