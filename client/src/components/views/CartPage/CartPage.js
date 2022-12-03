2da86ec0ea9cf6661a9d9d415940ed3811ad5ce2
Mintchodan <z78z@naver.com>
2022-12-03 23:56:24 +0900

    total price

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -3,34 +3,50 @@ import { useDispatch } from "react-redux";
 import { getCartItems } from "../../../_actions/user_action";
 import UserCardBlock from "./Sections/UserCardBlock";
 function CartPage(props) {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        let cartItems = [];
-        // 리덕스 User State안에 Cart 안에 상품이 들어있는지 확인
-        if (props.user.userData && props.user.userData.cart) {
-            if (props.user.userData.cart.length > 0) {
-                props.user.userData.cart.forEach((item) => {
-                    cartItems.push(item.id);
-                });
+  const [Total, setTotal] = useState(0);
 
-                dispatch(
-                    getCartItems(cartItems, props.user.userData.cart)
-                ).then((response) => {
-                    console.log(response);
-                });
-            }
-        }
-    }, [props.user.userData]);
+  useEffect(() => {
+    let cartItems = [];
+    // 리덕스 User State안에 Cart 안에 상품이 들어있는지 확인
+    if (props.user.userData && props.user.userData.cart) {
+      if (props.user.userData.cart.length > 0) {
+        props.user.userData.cart.forEach((item) => {
+          cartItems.push(item.id);
+        });
 
-    return (
-        <div style={{ width: "85%", margin: "3rem auto" }}>
-            <h1>My Cart</h1>
-            <div>
-                <UserCardBlock products={props.user && props.user.cartDetail} />
-            </div>
-        </div>
-    );
+        dispatch(getCartItems(cartItems, props.user.userData.cart)).then(
+          (response) => {
+            calculateTotal(response.payload);
+          }
+        );
+      }
+    }
+  }, [props.user.userData]);
+
+  let calculateTotal = (cartDetail) => {
+    let total = 0;
+
+    cartDetail.map((item) => {
+      total += parseInt(item.price, 10) * item.quantity;
+    });
+
+    setTotal(total);
+  };
+
+  return (
+    <div style={{ width: "85%", margin: "3rem auto" }}>
+      <h1>My Cart</h1>
+      <div>
+        <UserCardBlock products={props.user && props.user.cartDetail} />
+      </div>
+
+      <div style={{ marginTop: "3rem" }}>
+        <h2> Total Amount: ${Total}</h2>
+      </div>
+    </div>
+  );
 }
 
 export default CartPage;
