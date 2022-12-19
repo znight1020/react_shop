e5cc7ec9d07f680ae7e3739481687fb93e3737d5
현수 <znight1020@naver.com>
2022-12-19 22:36:44 +0900

    AppURL

--- client/src/components/views/Footer/Footer.js
+++ client/src/components/views/Footer/Footer.js
@@ -17,8 +17,10 @@ function Footer(props) {
         >
             <p>
                 {" "}
-                프리캡스톤디자인 5조 <SmileOutlined />
+                APK를 다운받고 핸드폰에서 AR 피팅 프로그램을 실행시켜보세요! &nbsp;&nbsp;&nbsp;
+                <a href="//www.naver.com"> www.naver.com </a> 
             </p>
+            <p>프리캡스톤디자인 5조 <SmileOutlined /></p>
         </div>
     );
 }
